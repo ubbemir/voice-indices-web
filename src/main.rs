2aad34861c09bee040e19b6926588b63fffc9462
Ubbemir <ubbemir@example.com>
2026-06-22 22:44:00 +0200

    initial mvp to upload a demo file, parse it and display some player info

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use demo::demo::extract_player_info;
+use js_sys;
 use leptos::prelude::*;
 use leptos::task::spawn_local;
 use web_sys::{HtmlInputElement, wasm_bindgen::JsCast};
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = signal(0);
+    let (player_info, set_player_info) = signal::<Option<String>>(None);
 
     let on_file_change = move |ev: leptos::ev::Event| {
         let input: HtmlInputElement = ev.target().unwrap().unchecked_into();
@@ -13,15 +15,51 @@ fn App() -> impl IntoView {
                 // e.g., file.name(), file.size()
                 let promise = file.bytes(); // or file.array_buffer()
                 spawn_local(async move {
-                    let text = wasm_bindgen_futures::JsFuture::from(promise).await.unwrap();
-                    // Process the file content here
+                    let bytes_value = wasm_bindgen_futures::JsFuture::from(promise).await.unwrap();
+                    let byte_array = js_sys::Uint8Array::new(&bytes_value);
+                    let mut bytes = vec![0; byte_array.length() as usize];
+                    byte_array.copy_to(&mut bytes);
+
+                    // Parse the demo file
+                    match extract_player_info(&bytes) {
+                        Ok(players) => {
+                            let info = players
+                                .iter()
+                                .map(|p| {
+                                    format!(
+                                        "{} (Slot: {}, Team: {}, SteamID: {})",
+                                        p.name, p.slot, p.team_number, p.steamid
+                                    )
+                                })
+                                .collect::<Vec<_>>()
+                                .join("\n");
+                            set_player_info.set(Some(info));
+                        }
+                        Err(e) => {
+                            set_player_info.set(Some(format!("Error parsing demo: {}", e)));
+                        }
+                    }
                 });
             }
         }
     };
 
     view! {
-        <input type="file" on:change=on_file_change/>
+        <div>
+            <h1>"Demo Parser"</h1>
+            <input type="file" on:change=on_file_change/>
+            {move || {
+                if let Some(info) = player_info.get() {
+                    view! {
+                        <pre>{info}</pre>
+                    }
+                } else {
+                    view! {
+                        <pre>{"No demo file loaded".to_string()}</pre>
+                    }
+                }
+            }}
+        </div>
     }
 }
 
