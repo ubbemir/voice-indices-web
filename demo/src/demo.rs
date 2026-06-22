b03e53d9cf81af77a006e747f221026bde9702de
Ubbemir <ubbemir@example.com>
2026-06-22 22:17:53 +0200

    make wasm compilation work with demo parser

---
+++ demo/src/demo.rs
@@ -0,0 +1,112 @@
+#![allow(dead_code)]
+
+use std::path::Path;
+
+use ahash::AHashMap;
+use csgoproto::CsvcMsgVoiceData;
+use parser::{
+    first_pass::{
+        parser_settings::{ParserInputs, create_mmap},
+        read_bits::DemoParserError,
+    },
+    parse_demo::Parser,
+    second_pass::parser_settings::{PlayerEndMetaData, create_huffman_lookup_table},
+};
+use thiserror::Error;
+
+#[derive(Debug)]
+pub struct PlayerData {
+    pub index: usize,
+    pub slot: usize,
+    pub steamid: u64,
+    pub name: String,
+    pub team_number: i32,
+}
+
+impl std::fmt::Display for PlayerData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{} - {}", self.name, self.slot)
+    }
+}
+
+impl PlayerData {
+    fn try_from(md: PlayerEndMetaData, index: usize, slot_offset: usize) -> Result<Self, Error> {
+        Ok(PlayerData {
+            index,
+            slot: slot_from_index(index, slot_offset),
+            steamid: md.steamid.ok_or(Error::MissingPlayerField("steamid"))?,
+            name: md.name.ok_or(Error::MissingPlayerField("name"))?,
+            team_number: md
+                .team_number
+                .ok_or(Error::MissingPlayerField("team_number"))?,
+        })
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("parsing error")]
+    DemoParserError(#[from] DemoParserError),
+
+    #[error("unable to compute a slot offset for players")]
+    UnattainableSlotOffset,
+
+    #[error("player data in demo is missing a crucial field: {0}")]
+    MissingPlayerField(&'static str),
+}
+
+pub fn extract_player_info(demo_path: &Path) -> Result<Vec<PlayerData>, Error> {
+    let settings = ParserInputs {
+        real_name_to_og_name: AHashMap::default(),
+        wanted_players: vec![],
+        wanted_player_props: vec![],
+        wanted_other_props: vec![],
+        wanted_prop_states: AHashMap::default(),
+        wanted_events: vec![],
+        parse_ents: false,
+        wanted_ticks: vec![],
+        parse_projectiles: false,
+        parse_grenades: false,
+        only_header: true,
+        list_props: false,
+        only_convars: false,
+        huffman_lookup_table: &create_huffman_lookup_table(),
+        order_by_steamid: false,
+        fallback_bytes: None,
+    };
+    let mmap = create_mmap(demo_path.display().to_string())?;
+    let mut parser = Parser::new(settings, parser::parse_demo::ParsingMode::Normal);
+    let output = parser.parse_demo(&mmap)?;
+
+    let voice_sample = &output.voice_data[0].1;
+    let slot_offset = get_player_slot_offset(output.player_md.iter(), voice_sample)
+        .ok_or(Error::UnattainableSlotOffset)?;
+
+    let players = output
+        .player_md
+        .iter()
+        .enumerate()
+        .map(|(index, p)| PlayerData::try_from(p.clone(), index, slot_offset))
+        .collect::<Result<_, Error>>()?;
+
+    Ok(players)
+}
+
+fn slot_from_index(index: usize, slot_offset: usize) -> usize {
+    const ONE_BASED_INDEXING_OFFSET: usize = 1;
+
+    index + slot_offset + ONE_BASED_INDEXING_OFFSET
+}
+
+fn get_player_slot_offset<'a>(
+    players: impl Iterator<Item = &'a PlayerEndMetaData>,
+    voice_sample: &CsvcMsgVoiceData,
+) -> Option<usize> {
+    let (index, _) = players
+        .enumerate()
+        .find(|(_, p)| p.steamid == voice_sample.xuid)?;
+
+    let client_idx = voice_sample.client?;
+
+    (client_idx - index as i32).try_into().ok()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use leptos::prelude::*;
 
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button
+            on:click=move |_| set_count.set(3)
+        >
+            "Click me: "
+            {count}
+        </button>
+        <p>
+            "Double count: "
+            {move || count.get() * 2}
+        </p>
+    }
+}
+
 fn main() {
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    mount_to_body(App);
 }
