479831f1ed634f2a083d917d402a18a640fff547
Ubbemir <ubbemir@example.com>
2026-06-22 22:36:50 +0200

    initial file selection

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 use leptos::prelude::*;
+use leptos::task::spawn_local;
+use web_sys::{HtmlInputElement, wasm_bindgen::JsCast};
 
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
 
+    let on_file_change = move |ev: leptos::ev::Event| {
+        let input: HtmlInputElement = ev.target().unwrap().unchecked_into();
+        if let Some(file_list) = input.files() {
+            if let Some(file) = file_list.get(0) {
+                // e.g., file.name(), file.size()
+                let promise = file.bytes(); // or file.array_buffer()
+                spawn_local(async move {
+                    let text = wasm_bindgen_futures::JsFuture::from(promise).await.unwrap();
+                    // Process the file content here
+                });
+            }
+        }
+    };
+
     view! {
-        <button
-            on:click=move |_| set_count.set(3)
-        >
-            "Click me: "
-            {count}
-        </button>
-        <p>
-            "Double count: "
-            {move || count.get() * 2}
-        </p>
+        <input type="file" on:change=on_file_change/>
     }
 }
 
