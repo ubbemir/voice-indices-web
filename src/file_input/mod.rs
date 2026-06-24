266b6741f2520751a27e060f6d6dd8eee68aa221
Ubbemir <ubbemir@example.com>
2026-06-25 00:03:05 +0200

    add a status message when parsing demo files

--- src/file_input/mod.rs
+++ src/file_input/mod.rs
@@ -10,6 +10,12 @@ pub type PlayerInfo = Vec<PlayerData>;
 #[component]
 pub fn DemoFileInput(mut on_player_info: impl FnMut(PlayerInfo) + 'static) -> impl IntoView {
     let (demo_parse_process, set_demo_parse_process) = signal::<Option<_>>(None);
+    let parse_message = move || {
+        demo_parse_process
+            .get()
+            .is_some()
+            .then_some("Parsing demo ...")
+    };
 
     let on_file_change = move |ev: leptos::ev::Event| {
         let input: HtmlInputElement = ev.target().unwrap().unchecked_into();
@@ -46,9 +52,11 @@ pub fn DemoFileInput(mut on_player_info: impl FnMut(PlayerInfo) + 'static) -> im
         };
 
         on_player_info(player_info);
+        set_demo_parse_process.set(None);
     });
 
     view! {
         <input type="file" on:change=on_file_change/>
+        <strong>{parse_message}</strong>
     }
 }
