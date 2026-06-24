eef3b051acc6b3d63f6ac12fd07495580f34ae00
Ubbemir <ubbemir@example.com>
2026-06-24 20:51:19 +0200

    clippy

--- src/file_input.rs
+++ src/file_input.rs
@@ -1,5 +1,4 @@
 use demo::demo::{PlayerData, extract_player_info};
-use js_sys;
 use leptos::prelude::*;
 use leptos::task::spawn_local;
 use web_sys::{HtmlInputElement, wasm_bindgen::JsCast};
@@ -10,23 +9,20 @@ pub type PlayerInfo = Vec<PlayerData>;
 pub fn DemoFileInput(set_player_info: WriteSignal<Option<PlayerInfo>>) -> impl IntoView {
     let on_file_change = move |ev: leptos::ev::Event| {
         let input: HtmlInputElement = ev.target().unwrap().unchecked_into();
-        if let Some(file_list) = input.files() {
-            if let Some(file) = file_list.get(0) {
-                let promise = file.bytes();
-                spawn_local(async move {
-                    let bytes_value = wasm_bindgen_futures::JsFuture::from(promise).await.unwrap();
-                    let byte_array = js_sys::Uint8Array::new(&bytes_value);
-                    let mut bytes = vec![0; byte_array.length() as usize];
-                    byte_array.copy_to(&mut bytes);
+        if let Some(file_list) = input.files()
+            && let Some(file) = file_list.get(0)
+        {
+            let promise = file.bytes();
+            spawn_local(async move {
+                let bytes_value = wasm_bindgen_futures::JsFuture::from(promise).await.unwrap();
+                let byte_array = js_sys::Uint8Array::new(&bytes_value);
+                let mut bytes = vec![0; byte_array.length() as usize];
+                byte_array.copy_to(&mut bytes);
 
-                    match extract_player_info(&bytes) {
-                        Ok(players) => {
-                            set_player_info.set(Some(players));
-                        }
-                        Err(_) => (),
-                    }
-                });
-            }
+                if let Ok(players) = extract_player_info(&bytes) {
+                    set_player_info.set(Some(players));
+                }
+            });
         }
     };
 
